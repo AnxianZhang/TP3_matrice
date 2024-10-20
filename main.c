e7f023815a094b43d475c75bf2c7ff35aa8d12ff
AnxianZhang <gadanxianzhang@gmail.com>
2024-10-20 19:35:51 +0200

    Added the possibility to enter several matrix and manipulate them

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "SparseMatrix.h"
 #include "Utile.h"
 
@@ -12,8 +13,32 @@ int getUserNumber(const char *message) {
     return number;
 }
 
+int getNumberOfMatrix(unsigned int currentNumberOfMatrix) {
+    int value = getUserNumber("Which matrix you want to use for ?");
+    if (value <= 0 || value > currentNumberOfMatrix) return -1;
+    return value;
+}
+
+void manageMatrixArray(SparseMatrix ***m, unsigned int *max, const unsigned int *current) {
+    if (*current == *max) {
+        *max += 5;
+        SparseMatrix **newM = malloc(sizeof(SparseMatrix *) * *max);
+
+        for (unsigned int i = 0; i < *current; ++i)
+            newM[i] = (*m)[i];
+
+        // TODO: freeMatrix();
+        free(*m);
+
+        *m = newM;
+    }
+}
+
 int main() {
-    SparseMatrix m;
+    int selectedMatrix;
+    unsigned int maxSize = 2, usedSize = 0;
+
+    SparseMatrix **m = malloc(sizeof(SparseMatrix *) * maxSize);
 
     // ============= UI ============= */
     char choice = '0';
@@ -36,27 +61,39 @@ int main() {
                 line = getUserNumber("Please enter the number of lines for the matrix:");
                 column = getUserNumber("Please enter the number of columns for the matrix:");
 
-                populateMatrix(&m, line, column);
+                manageMatrixArray(&m, &maxSize, &usedSize);
+                m[usedSize] = malloc(sizeof(SparseMatrix));
+                populateMatrix(m[usedSize++], line, column);
                 break;
-            case '2':
-                // TODO: enable the possibility to créate severa matrix and chose which one to show
-                showMatrix(&m);
 
+            case '2':
+                selectedMatrix = getNumberOfMatrix(usedSize);
+
+                if (selectedMatrix == -1)
+                    printf("Invalid value, please retry");
+                else {
+                    printf("\n-----> Matrix number %d <-----\n", selectedMatrix);
+                    showMatrix(m[selectedMatrix - 1]);
+                }
                 break;
 
             case '3':
                 break;
 
             case '4':
-                line = getUserNumber("Please enter the number of line that you searching for:");
-                column = getUserNumber("Please enter the number of column that you searching for:");
+                selectedMatrix = getNumberOfMatrix(usedSize);
+                if (selectedMatrix == -1)
+                    printf("Invalid value, please retry");
+                else {
+                    line = getUserNumber("Please enter the number of line that you searching for:");
+                    column = getUserNumber("Please enter the number of column that you searching for:");
 
-                const int value = searchValue(&m, line, column);
-                if (value == -1)
-                    printf("The value doesn't exist for line %d, column %d! \n", line, column);
-                else
-                    printf("Value at line %d, column %d = %d\n", line, column, value);
-
+                    const int value = searchValue(m[selectedMatrix - 1], line, column);
+                    if (value == -1)
+                        printf("The value doesn't exist for line %d, column %d! \n", line, column);
+                    else
+                        printf("Value at line %d, column %d = %d\n", line, column, value);
+                }
                 break;
 
             case '5':
@@ -77,6 +114,9 @@ int main() {
         }
         emptyBuffer();
     }
+
+    // TODO freeMatrix();
+    free(m);
 
     return 0;
 }
